1b618b96dada1b3af1a9393744b6718b1d75b620
Kouki Yanai <e205702@ie.u-ryukyu.ac.jp>
2022-01-30 20:36:42 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fileWrite
+# fileWrite
+# fileWrite
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+    "github.com/e205702/fileWrite/fileWrite"
+)
+
+func main() {
+    fmt.Println(fileWrite2.Hello("kyoro"))
+}
